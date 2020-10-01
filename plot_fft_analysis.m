1069f8ade8575effc458c23042dc43ff055a67a0
Abhijith Mundanad <mabhijithn@gmail.com>
2020-10-01 11:21:08 +0200

    Function to plot FFT of alpha experiment

---
+++ plot_fft_analysis.m
@@ -0,0 +1,33 @@
+function plot_fft_analysis(fftstoplot, savefig, savepath)
+    if(nargin>1)
+        if(savefig)
+            if(isempty(savepath))
+                mkdir('tmpfig');
+                savepath = 'tmpfig';
+            end
+        end
+    else
+        savefig = 0;
+    end
+    
+    for i = 1:size(fftstoplot,2)
+        L = fftstoplot(i).L;
+        L_by_2 = floor(L/2);
+        fftdata = fftstoplot(i).data;
+        title_str = fftstoplot(i).title;
+        fs = fftstoplot(i).fs;
+        savestr = fftstoplot(i).savestr;
+
+        %calculate frequency bins with FFT
+        df=fs/L; %frequency resolution
+        sampleIndex = 0:L-1; %raw index for FFT plot
+        f=sampleIndex*df; %x-axis index converted to frequencies
+        plot(f(1:L_by_2),abs(fftdata(1:L_by_2)));
+        ylabel('FFT Magnitude (Avg)');
+        xlabel('Frequency (Hz)');
+        title(title_str);
+        if(savefig)
+            saveas(gcf, fullfile(savepath,savestr),'jpg');
+        end
+    end
+end
