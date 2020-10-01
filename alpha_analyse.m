b740b75e89c61388a04957f50be21f6db809f2f0
Abhijith Mundanad <mabhijithn@gmail.com>
2020-10-01 11:21:50 +0200

    Alpha analysis script function input args modified

--- alpha_analyse.m
+++ alpha_analyse.m
@@ -1,12 +1,21 @@
-function [avg_fft_open_1,avg_fft_open_2,avg_fft_close] = alpha_analyse(EEG, fs_new)
+function [avg_fft_open_1,avg_fft_open_2,avg_fft_close] = alpha_analyse(EEG, fs_new,dur)
     EEG = pop_resample(EEG, fs_new);
     EEG = pop_eegfiltnew(EEG, [],0.5,[],true); %HP filtering (Filter DC)
     EEG = pop_eegfiltnew(EEG, [],20); %LP filtering
     EEGfilt = EEG.data(1,:)';
     
-    eye_open_1_erc = EEGfilt(16*fs_new+1:76*fs_new);
-    eye_close_erc = EEGfilt(76*fs_new+1:136*fs_new);
-    eye_open_2_erc = EEGfilt(136*fs_new+1:196*fs_new);
+    strt = 16;
+%     dur = 60;
+    stp = strt+dur;
+    eye_open_1_erc = EEGfilt(strt*fs_new+1:stp*fs_new);
+    
+    strt = stp;
+    stp = strt+dur;
+    eye_close_erc = EEGfilt(strt*fs_new+1:stp*fs_new);
+
+    strt = stp;
+    stp = strt+dur;
+    eye_open_2_erc = EEGfilt(strt*fs_new+1:stp*fs_new);
     
     strt = 1;
     dur = 2048e-3;
